29570648989ac18a2be5cf36f0242ec9f3cdd44f
Olawale <olapetanwale@outlook.com>
2022-07-19 18:32:00 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/index';
+import store from './Redux/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
