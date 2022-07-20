1eeac67f116184cbc9d7d5e81fc782f808403202
Olawale <olapetanwale@outlook.com>
2022-07-20 13:00:17 +0100

    Implement rocket list render

--- src/components/RocketCard.js
+++ src/components/RocketCard.js
@@ -6,17 +6,20 @@ import { rocketActions } from '../Redux/RocketSlice';
 const RocketCard = ({ data: rocket }) => {
   // const rockets = useSelector((state) => state.rocket.isReserved);
   const dispatch = useDispatch();
+  const {
+    rocketName, description, flickrImages,
+  } = rocket;
 
-  const clickHandler = (event) => {
-    event.preventDefault();
-    dispatch(rocketActions.reseverd(rocket));
+  const clickHandler = () => {
+    dispatch(rocketActions.reseverd({ rocket }));
   };
+
   return (
     <div className="rocket-card">
-      <img style={{ width: '200px' }} src={rocket.flickr_images} alt="" />
+      <img style={{ width: '200px' }} src={flickrImages} alt="" />
       <div>
-        <h3>{rocket.rocketName}</h3>
-        <p>{rocket.description}</p>
+        <h3>{rocketName}</h3>
+        <p>{description}</p>
         <button className="rocket-btn" onClick={clickHandler} type="submit">Reserve Rocket</button>
       </div>
     </div>
